b57e9fc11b4217c180dc279985ac76c3dfce66a3
Enkang Yuan <enkyuan@Enkangs-MacBook-Pro.local>
2024-03-03 12:44:09 -0600

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from "./src/pages/Home";
+import SignIn from "./src/pages/SignIn";
+import Signup from "./src/pages/SignUp";
+import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import tw from "twrnc";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={tw`flex-1 items-center jusify-center`}>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
