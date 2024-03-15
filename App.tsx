3548c05405b0ccf63e75fda3a46d778fd623f5fe
Enkang Yuan <yuan_enkang@outlook.com>
2024-03-15 18:30:36 -0500

    added ios compatibility

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
-import SignIn from "./src/pages/SignIn";
+import SignIn from "./src/pages/signin";
 import { View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import tw from "twrnc";
-import SignUp from "./src/pages/SignUp";
+import SignUp from "./src/pages/signup";
+import Home from "./src/pages/home";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <View style={tw`flex-1 items-center justify-center`}>
-      <SignIn />
+    <GestureHandlerRootView style={tw`flex-1`}>
+      <Home />
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,19 @@
-import { SafeAreaView, View } from "react-native";
-import React from "react";
+import React, { useMemo } from "react";
+import { Pressable, View, Text, FlatList } from "react-native";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import tw from "twrnc";
 
 export default function Home() {
+  const snapPoints = useMemo(() => ["25%", "50%"], []);
+
   return (
-    <SafeAreaView>
-      <View></View>
-    </SafeAreaView>
+    <View style={tw`flex-1 p-24 bg-gray-200`}>
+      <BottomSheet snapPoints={snapPoints}>
+        <FlatList horizontal={true}>
+          <Pressable style={tw`bg-gray-400 m-2 rounded-lg`}></Pressable>
+          <Pressable></Pressable>
+        </FlatList>
+      </BottomSheet>
+    </View>
   );
 }
