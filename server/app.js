89cc5dc7556c1665b88c7073071bd7a11b1b936c
Enkang Yuan <yuan_enkang@outlook.com>
2024-03-20 15:43:38 -0500

    modified public directory

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
+import stripe from "stripe";
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT;
-const key = process.env.STRIPE_KEY;
-const secret = process.env.STRIPE_SECRET;
+// const KEY = process.env.STRIPE_KEY;
+// const SECRET = process.env.STRIPE_SECRET;
 
 app.listen(PORT, () => {
   console.log("Server Listening on port", PORT);
